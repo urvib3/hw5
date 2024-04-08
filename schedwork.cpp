d2bc411dab447d67544db0a63ef61a81608d2938
Urvi Bhuwania <ubhuwani@usc.edu>
2024-04-08 20:26:04 +0000

    wordle + schedwork implemented

--- schedwork.cpp
+++ schedwork.cpp
@@ -6,6 +6,9 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <bitset>
+#include <climits>
+#include <cstring>
 // add or remove necessary headers as you please
 
 #endif
@@ -21,6 +24,27 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool schedHelp(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    const size_t totalWorkers, 
+    int* shifts,            // number of shifts each worker is working so far
+    int cDay, int cPos      // current position in schedule
+);
+
+// Checks if inserting a certain worker into a certain position is isValid
+// 1) Worker must be available
+// 2) Worker must not already be scheduled for the same day 
+// 3) Worker must be under max shifts 
+bool isValid(const AvailabilityMatrix& avail,
+    const size_t maxShifts,
+    int* shifts,            // number of shifts each worker is working so far
+    int cDay,     // current position in schedule
+    int cWorkerID           // worker to check
+);
+
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -35,11 +59,89 @@ bool schedule(
     if(avail.size() == 0U){
         return false;
     }
+
     sched.clear();
     // Add your code below
 
+    int k = avail[0].size(); // total # of workers
+    int* shifts = new int[k]; 
+    for(int i = 0; i < k; i++) shifts[i] = 0; 
 
+    // initialize sched
+    sched = std::vector<std::vector<Worker_T>>(avail.size(), std::vector<Worker_T>(dailyNeed, -1));
 
+    bool ans = schedHelp(avail, dailyNeed, maxShifts, sched, k, shifts, 0, 0); 
+    
+    delete [] shifts; 
+
+    return ans; 
 
 }
+
+
+bool schedHelp(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    const size_t totalWorkers, 
+    int* shifts,            // number of shifts each worker is working so far
+    int cDay, int cPos      // current position in schedule
+) {
+
+  // std::cout << "cDay: " << cDay << " cPos: " << cPos << std::endl; 
+  // std::cout << "printing sched " << sched.size() << std::endl; 
+  // for(std::vector<Worker_T> v : sched) { for (Worker_T w : v) std:: cout << w << " "; std::cout << std::endl; }
+
+  // BASE CASE: cDay and cPos out of bounds of schedule array
+  if(cDay >= sched.size()) return true; 
+
+  // Try every possible worker for current day and current position in schedule
+  // To avoid repeat combinations, all workers for a day will be tried in ascending order
+  for(int cWorkerID = (cPos > 0) ? sched[cDay][cPos - 1] + 1 : 0; cWorkerID < totalWorkers; cWorkerID++) {
+    
+    // only update data and recurse if current worker is a possibility
+    if(isValid(avail, maxShifts, shifts, cDay, cWorkerID)) {
+
+      // update shifts array and schedule
+      shifts[cWorkerID]++; 
+      sched[cDay][cPos] = cWorkerID; 
+
+      // recurse and return true if recursion leads to a solution
+      if(schedHelp(avail, dailyNeed, maxShifts, sched, totalWorkers, shifts, 
+      (cPos + 1 == dailyNeed) ? cDay + 1 : cDay, 
+      (cPos + 1 == dailyNeed) ? 0 : cPos + 1)) return true; 
+
+      // backtrack 
+      shifts[cWorkerID]--; 
+      sched[cDay][cPos] = -1; // optional reset
+    }
+  }
+
+  return false; 
+}
+
+
+bool isValid(const AvailabilityMatrix& avail,
+    const size_t maxShifts, 
+    int* shifts,            // number of shifts each worker is working so far
+    int cDay,               // current position in schedule
+    int cWorkerID           // worker to check
+) {
+
+  // 1) Worker must be available
+  // 2) Worker must be under max shifts 
+
+  if(!avail[cDay][cWorkerID] || 
+      shifts[cWorkerID] == maxShifts) return false; 
+
+  return true; 
+
+}
+
+
+
+
+
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,12 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelp(
+    std::string& in,
+    const std::string floating,
+    const std::set<std::string>& dict,
+    std::set<std::string>& ans,
+    const int blanks); 
 
 
 // Definition of primary wordle function
@@ -21,8 +27,55 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+
+  // count blanks of word
+  int blanks = 0; 
+  for(char c : in) if(c == '-') blanks++;     
+
+   std::set<std::string> ans; 
+   std::string inCopy = in; 
+   wordleHelp(inCopy, floating, dict, ans, blanks); 
+
+   return ans;
 
 }
 
 // Define any helper functions here
+void wordleHelp(
+    std::string &in,
+    const std::string floating,
+    const std::set<std::string>& dict,
+    std::set<std::string>& ans,
+    const int blanks)
+{
+
+
+    // check base case
+    if(blanks == 0) {
+        if(dict.count(in)) ans.insert(in); 
+        return; 
+    }
+
+    // if blanks > floating size, try every single letter
+    if(blanks > floating.size()) {
+        int index = in.find_first_of('-'); 
+        for(char c = 'a'; c <= 'z'; c++) {
+            in[index] = c; 
+            // if floating has current char, delete it from floating
+            wordleHelp(in, (floating.find_first_of(c) != std::string::npos) ? ((floating.substr(0, floating.find_first_of(c)) + floating.substr(floating.find_first_of(c) + 1))) : floating, dict, ans, blanks - 1);  
+            in[index] = '-';
+        }
+    }
+    
+    // if blanks = floating size, try every single floating letter
+    else if(blanks == floating.size()){
+        int index = in.find_first_of('-');
+        for(char c : floating) {
+            in[index] = c; 
+            // if floating has current char, delete it from floating
+            wordleHelp(in, (floating.substr(0, floating.find_first_of(c)) + floating.substr(floating.find_first_of(c) + 1)), dict, ans, blanks - 1); 
+            in[index] = '-';
+        }
+    }
+
+}
